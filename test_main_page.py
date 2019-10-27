5fd5304678ea101aca460901f6ec40eb4dd5bb79
mighty_maria <m.bezymyannaya.com>
2019-10-27 19:29:00 -0400

    Add main_page.py and changes to  test_main_page.py and base_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,20 @@
-def test_guest_can_go_to_login_page(browser):
+from .pages.main_page import MainPage
+from selenium.common.exceptions import NoSuchElementException
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page = MainPage(browser, link)
+#     # открываем страницу
+#     page.open()
+#     # выполняем метод страницы - переходим на страницу логина
+#     page.go_to_login_page()
+
+def test_guest_should_be_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
