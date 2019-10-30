faedbf5e0ad288029d873aa8cc40eb79425c7d4c
mighty_maria <m.bezymyannaya.com>
2019-10-30 19:17:05 -0400

    Fix a typo

--- test_main_page.py
+++ test_main_page.py
@@ -11,7 +11,7 @@ from selenium.common.exceptions import NoSuchElementException
 #     # выполняем метод страницы - переходим на страницу логина
 #     page.go_to_login_page()
 
-def test_guest_should_be_login_link(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
