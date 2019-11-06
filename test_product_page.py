d87917fc9c5f1297119cc3807693b7628fd0ded5
mighty_maria <m.bezymyannaya.com>
2019-11-05 20:04:48 -0500

    Add parametrization

--- test_product_page.py
+++ test_product_page.py
@@ -1,14 +1,16 @@
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 import time
+import pytest
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+@pytest.mark.parametrize('promo_offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "7" "8", "9"])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_be_added_to_basket_message()
     page.should_be_cost_of_the_basket_message()
-    time.sleep(10)
-
+    # time.sleep(1)
