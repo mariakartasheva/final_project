840c74e5d5e1f57b6ee79d3e724c322f40406c8e
mighty_maria <m.bezymyannaya.com>
2019-11-20 20:26:54 -0500

    Delete unnecessary markers

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,6 @@ from selenium.common.exceptions import NoSuchElementException
 import pytest
 
 
-@pytest.mark.login_guest
 class TestLoginFromMainPage():
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
--- test_product_page.py
+++ test_product_page.py
@@ -70,7 +70,6 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page.should_be_basket_is_empty_message()
 
 
-@pytest.mark.dev
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, browser):
